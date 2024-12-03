1922666b686cb956cf87f5d78f12634d863b7489
dev-jane99 <ruejaeeun99@gmail.com>
2024-12-03 16:22:22 +0900

    mapper

---
+++ src/main/java/com/app/springpowpow/domain/PetsonalVO.java
@@ -0,0 +1,15 @@
+package com.app.springpowpow.domain;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@Data
+//@Schema(description = "회원 정보")
+public class PetsonalVO implements Serializable {
+//    @Schema(description = "회원 번호", example = "21", required = true)
+//    private Long id;
+
+}
