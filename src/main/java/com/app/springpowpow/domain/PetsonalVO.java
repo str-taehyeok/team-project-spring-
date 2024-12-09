401fc3d487884f762712886537875d1ee8739ad9
str-taehyeok <gimtaehyeog63@gmail.com>
2024-12-09 19:32:03 +0900

    멤버, 펫스널 vo dto 추가

--- src/main/java/com/app/springpowpow/domain/PetsonalVO.java
+++ src/main/java/com/app/springpowpow/domain/PetsonalVO.java
@@ -12,14 +12,24 @@ import java.io.Serializable;
 public class PetsonalVO implements Serializable {
     @Schema(description = "펫스널 번호", example = "21", required = true)
     private Long id;
+    @Schema(description = "펫 번호", example = "21", required = true)
     private Long petId;
+    @Schema(description = "회원 번호", example = "반려견", required = true)
     private Long memberId;
+    @Schema(description = "시크 점수", example = "1")
     private int petsonalChic;
+    @Schema(description = "귀여움 점수", example = "1")
     private int petsonalCute;
+    @Schema(description = "차분함 점수", example = "1")
     private int petsonalCalm;
+    @Schema(description = "활동력 점수", example = "1")
     private int petsonalActive;
+    @Schema(description = "게으름 점수", example = "1")
     private int petsonalLazy;
+    @Schema(description = "부지런함 점수", example = "1")
     private int petsonalDiligent;
+    @Schema(description = "겁 점수", example = "1")
     private int petsonalCoward;
+    @Schema(description = "용감함 점수", example = "1")
     private int petsonalBrave;
 }
---
+++ src/main/java/com/app/springpowpow/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.app.springpowpow.service;
+
+import com.app.springpowpow.domain.MemberVO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberService {
+    public Optional<MemberVO> getMemberById(Long id);
+
+    public List<MemberVO> getAllMembers();
+
+    public Long getMemberIdByEmail(String memberEmail);
+
+    public void register(MemberVO memberVO);
+
+    public void modify(MemberVO memberVO);
+
+    public void withdraw(Long id);
+}
