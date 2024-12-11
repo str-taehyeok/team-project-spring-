62446459e66acf6862be06f29015d46ece385044
dev-jane99 <ruejaeeun99@gmail.com>
2024-12-11 16:14:25 +0900

    dao

--- src/main/java/com/app/springpowpow/mapper/ProductMapper.java
+++ src/main/java/com/app/springpowpow/mapper/ProductMapper.java
@@ -1,7 +1,24 @@
 package com.app.springpowpow.mapper;
 
+import com.app.springpowpow.domain.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface ProductMapper {
+//    제품 추가
+    public void insert(ProductVO productVO);
+//    제품 단일
+    public Optional<ProductVO> select(Long id);
+//    제품 리스트
+    public List<ProductVO> selectAll();
+//    판매자의 모든 제품
+    public List<ProductVO> selectAllBySeller(ProductVO productVO);
+//    수정
+    public void update(ProductVO productVO);
+//    삭제
+    public void delete(Long id);
+
 }
