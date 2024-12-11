3113a5d81d0283a9b3070dd9ffbc86784201e202
soyeon-00 <soyeonlee0515@gmail.com>
2024-12-11 11:43:48 +0900

    공지사항 진행중..

--- src/main/java/com/app/springpowpow/domain/NoticeVO.java
+++ src/main/java/com/app/springpowpow/domain/NoticeVO.java
@@ -1,15 +1,26 @@
 package com.app.springpowpow.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Component
 @Data
-//@Schema(description = "회원 정보")
+@Schema(description = "공지사항 정보")
 public class NoticeVO implements Serializable {
-//    @Schema(description = "회원 번호", example = "21", required = true)
-//    private Long id;
 
+    @Schema(description = "공지사항 번호", example = "25", required = true)
+    private Long id;
+    @Schema(description = "공지사항 제목", example = "POWPOW 공지사항 제목", required = true)
+    private String noticeTitle;
+    @Schema(description = "공지사항 내용", example = "POWPOW 공지사항 내용", required = true)
+    private String noticeContent;
+    @Schema(description = "공지사항 등록일자", example = "2024-12-11", required = true)
+    private Date noticeDate;
+    @Schema(description = "공지사항 조회수", example = "1000")
+    private Long noticeCount;
+    
 }
