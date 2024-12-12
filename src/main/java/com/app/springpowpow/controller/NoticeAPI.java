96fb446d44486cd90bb0160de468ae6c58b2db59
soyeon-00 <soyeonlee0515@gmail.com>
2024-12-12 10:21:32 +0900

    공지사항 등록 백 완료

--- src/main/java/com/app/springpowpow/controller/NoticeAPI.java
+++ src/main/java/com/app/springpowpow/controller/NoticeAPI.java
@@ -1,7 +1,11 @@
 package com.app.springpowpow.controller;
 
+import com.app.springpowpow.domain.NoticeDTO;
 import com.app.springpowpow.domain.NoticeVO;
+import com.app.springpowpow.domain.PetDTO;
+import com.app.springpowpow.domain.PetVO;
 import com.app.springpowpow.service.NoticeService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -9,20 +13,23 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/admin/notice/*")
+@RequestMapping("/notice/*")
 public class NoticeAPI {
 
     private final NoticeService noticeService;
 
-
-//    공지사항 작성
+    //    공지사항 작성
     @Operation(summary = "공지사항 작성", description = "공지사항 새로 작성 할 수 있는 API")
     @Parameters({
             @Parameter(name = "id", description = "공지사항 번호", schema = @Schema(type = "number"), in = ParameterIn.HEADER, required = true),
@@ -32,10 +39,16 @@ public class NoticeAPI {
     })
     @ApiResponse(responseCode = "200", description = "공지사항 작성 완료")
     @PostMapping("write")
-    public NoticeVO write(@RequestBody NoticeVO noticeVO) {
+    public NoticeDTO write(@RequestBody NoticeVO noticeVO) {
+        log.info("{}", noticeVO.toString());
         noticeService.write(noticeVO);
-        return new NoticeVO();
+        Optional<NoticeDTO> foundNotice = noticeService.read(noticeVO.getId());
+        if(foundNotice.isPresent()){
+            return foundNotice.get();
+        }
+        return new NoticeDTO();
     }
+
 
 
 
--- src/main/java/com/app/springpowpow/repository/NoticeDAO.java
+++ src/main/java/com/app/springpowpow/repository/NoticeDAO.java
@@ -1,5 +1,6 @@
 package com.app.springpowpow.repository;
 
+import com.app.springpowpow.domain.NoticeDTO;
 import com.app.springpowpow.domain.NoticeVO;
 import com.app.springpowpow.mapper.NoticeMapper;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +21,12 @@ public class NoticeDAO {
     }
 
 //    공지사항 전체 리스트
-    public List<NoticeVO> findAll() {
+    public List<NoticeDTO> findAll() {
         return noticeMapper.selectAll();
     }
 
 //    공지사항 개별 리스트
-    public Optional<NoticeVO> findById(Long id) {
+    public Optional<NoticeDTO> findById(Long id) {
         return noticeMapper.select(id);
     }
 
