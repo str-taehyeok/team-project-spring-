75751f612306a65047c06479abffb7170134e5db
dev-cdy <argon4067@gmail.com>
2024-12-13 11:01:54 +0900

    temp

--- src/main/java/com/app/springpowpow/controller/CouponAPI.java
+++ src/main/java/com/app/springpowpow/controller/CouponAPI.java
@@ -30,7 +30,7 @@ public class CouponAPI {
     @Operation(summary = "쿠폰 생성", description = "쿠폰을 생성할 수 있는 API")
 //    @Parameters({
 //            @Parameter(name = "id", description = "쿠폰 번호", schema = @Schema(type = "number"), in = ParameterIn.HEADER, required = true),
-//            @Parameter(name = "memberId", description = "멤버 번호", schema = @Schema(type= "number"), in = ParameterIn.HEADER, required = true),
+//            @Parameter(name = "memberId", description = "멤버 번호", schema = @Schema(type= "number"), in = ParameterIn.HEADER),
 //            @Parameter(name = "couponTitle", description = "쿠폰 이름", schema = @Schema(type= "string"), in = ParameterIn.HEADER, required = true),
 //            @Parameter(name = "couponCategory", description = "쿠폰 카테고리", schema = @Schema(type= "string"), in = ParameterIn.HEADER, required = true),
 //            @Parameter(name = "couponCategoryAnimal", description = "쿠폰 동물 카테고리", schema = @Schema(type= "string"), in = ParameterIn.HEADER, required = true),
