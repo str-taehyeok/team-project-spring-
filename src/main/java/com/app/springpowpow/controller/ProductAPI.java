75e3093466756f79ffbffb924edccd11f4eb58c6
dev-jane99 <ruejaeeun99@gmail.com>
2024-12-13 10:59:00 +0900

    completed product and delivery

--- src/main/java/com/app/springpowpow/controller/ProductAPI.java
+++ src/main/java/com/app/springpowpow/controller/ProductAPI.java
@@ -27,24 +27,14 @@ public class ProductAPI {
 
 
 //    제품 등록
-//    @Operation(summary = "제품 등록", description = "제품등록 API")
-//    @ApiResponse(responseCode = "200", description = "제품등록 완료")
-//    @PostMapping("write")
-//    public void insert(@RequestBody ProductVO productVO) {
-//        productService.insertNewProduct(productVO);
-//    }
-
     @Operation(summary = "제품 등록", description = "제품등록 API")
     @ApiResponse(responseCode = "200", description = "제품등록 완료")
     @Parameters({
-            @Parameter(name = "id", description = "제품 번호", schema = @Schema(type = "number"), in = ParameterIn.HEADER, required = true),
             @Parameter(name = "memberId", description = "회원 id", schema = @Schema(type = "number"), in = ParameterIn.HEADER, required = true),
             @Parameter(name = "productName", description = "제품명", schema = @Schema(type = "string"), in = ParameterIn.HEADER, required = true),
             @Parameter(name = "productPrice", description = "제품가격", schema = @Schema(type = "number"), in = ParameterIn.HEADER),
             @Parameter(name = "productCode", description = "제품 코드", schema = @Schema(type = "string"), in = ParameterIn.HEADER, required = true),
-            @Parameter(name = "productDate", description = "제품 등록일자", schema = @Schema(type = "string", format = "date"), in = ParameterIn.HEADER),
             @Parameter(name = "productStock", description = "제품 재고", schema = @Schema(type = "number"), in = ParameterIn.HEADER),
-            @Parameter(name = "productEditDate", description = "제품 수정일자", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
             @Parameter(name = "productDetail", description = "제품 상세정보", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
             @Parameter(name = "productImage1", description = "제품 이미지1", schema = @Schema(type = "string"), in = ParameterIn.HEADER, required = true),
             @Parameter(name = "productImage2", description = "제품 이미지2", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
@@ -54,27 +44,11 @@ public class ProductAPI {
             @Parameter(name = "productCategory", description = "제품 카테고리", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
             @Parameter(name = "productColor", description = "제품 펫스널 컬러", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
             @Parameter(name = "productSize", description = "제품 사이즈", schema = @Schema(type = "char"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryFeeKind", description = "배송 ", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryFee", description = "배송 가격", schema = @Schema(type = "number"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryFeeFree", description = "얼마이상 무료배송", schema = @Schema(type = "number"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryHow", description = "배송 방법", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryPayWhen", description = "착불여부", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
-            @Parameter(name = "deliveryCompany", description = "택배사", schema = @Schema(type = "string"), in = ParameterIn.HEADER),
     })
     @PostMapping("write")
-    public ResponseEntity<ProductDTO> insert(
-            @RequestPart("productImage") MultipartFile mainImage,
-            @RequestPart(value = "productSubImage1", required = false) MultipartFile subImage1,
-            @RequestPart(value = "productSubImage2", required = false) MultipartFile subImage2,
-            @RequestPart(value = "productSubImage3", required = false) MultipartFile subImage3,
-            @RequestPart("productDTO") ProductDTO productDTO
-    ) {
-        productService.insertProduct(productDTO);
-        return ResponseEntity.ok(productDTO);
+    public void insert(@RequestBody ProductDTO productDTO) {
+        productService.insertNewProduct(productDTO);
     }
-
-
-
 
 
     @Operation(summary = "제품 전체 조회", description = "모든 제품을 리스트로 볼수 있는 API")
@@ -96,14 +70,10 @@ public class ProductAPI {
     @Parameter(name = "id", description = "제품 수정", schema = @Schema(type="number"))//DB의 스키마가 아니라, swagger에서 인식하기 위한 타입in = ParameterIn.PATH, //path 로 전달required = true //반드시 전달)
     @ApiResponse(responseCode = "200", description = "제품 수정 완료")
     @PutMapping("seller-product/{id}")
-    public ProductDTO modify(@PathVariable Long id, @RequestBody ProductDTO productDTO){
+    public ProductDTO modify(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
         productDTO.setId(id);
-        productService.updateProductAndDelivery(productDTO);
-        Optional<ProductDTO> foundProduct = productService.selectProductById(productDTO.getId());
-        if(foundProduct.isPresent()){
-            return foundProduct.get();
-        }
-        return new ProductDTO();
+        productService.updateProduct(productDTO);
+        return productDTO;
     }
 
 //    제품 삭제
