2bcf9ddafccf362c52760634bfc3a261db57068b
dev-minchul <minchul766@gmail.com>
2024-12-14 16:22:58 +0900

    success5

---
+++ src/main/java/com/app/springpowpow/domain/PostDTO.java
@@ -0,0 +1,40 @@
+package com.app.springpowpow.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+
+@Component
+@Data
+@Schema(description = "게시글 정보")
+public class PostDTO {
+    @Schema(description = "게시글 번호", example = "1", required = true)
+    private Long id;
+    @Schema(description = "회원 번호", example = "1", required = true)
+    private Long memberId;
+    @Schema(description = "게시글 제목", example = "테스트 제목1", required = true)
+    private String postTitle;
+    @Schema(description = "게시글 내용", example = "테스트 내용1", required = true)
+    private String postContent;
+    @Schema(description = "게시글 작성시간", example = "2024-12-24:00:10")
+    private LocalDateTime postCreateAt;
+    @Schema(description = "게시글 수정시간", example = "2024-12-24:00:10")
+    private LocalDateTime postModifiedAt;
+    @Schema(description = "게시글 이미지1", example = "post.png")
+    private String postImage1;
+    @Schema(description = "게시글 이미지2", example = "post.png")
+    private String postImage2;
+    @Schema(description = "게시글 이미지3", example = "post.png")
+    private String postImage3;
+    @Schema(description = "게시글 이미지4", example = "post.png")
+    private String postImage4;
+    @Schema(description = "게시글 이미지5", example = "post.png")
+    private String postImage5;
+    @Schema(description = "게시글 색상", example = "Gold")
+    private String postColor;
+    @Schema(description = "회원 이메일", example = "test1234@test.app")
+    private String memberEmail;
+    @Schema(description = "회원 이름", example = "아무개")
+    private String memberName;
+}
