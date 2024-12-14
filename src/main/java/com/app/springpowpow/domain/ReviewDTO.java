f634cff25e81bd7d147704923773344118371cc4
dev-jane99 <ruejaeeun99@gmail.com>
2024-12-14 19:28:47 +0900

    review

--- src/main/java/com/app/springpowpow/domain/ReviewDTO.java
+++ src/main/java/com/app/springpowpow/domain/ReviewDTO.java
@@ -4,12 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-import java.io.Serializable;
-
 @Component
 @Data
 //@Schema(description = "회원 정보")
-public class ReviewVO {
+public class ReviewDTO {
     @Schema(description = "리뷰 번호", example = "1", required = true)
     private Long id;
 
@@ -22,9 +20,16 @@ public class ReviewVO {
     @Schema(description = "리뷰 별점", example = "5", required = true)
     private int reviewStar;
 
-    @Schema(description = "리뷰 작성 날짜", example = "2024-06-14T15:30:00", required = true)
+    @Schema(description = "리뷰 작성 날짜", example = "2024-06-14T15:30:00")
     private String reviewDate;
 
     @Schema(description = "리뷰 내용", example = "정말 만족스러운 상품입니다!", required = true)
     private String reviewContent;
+
+    @Schema(description = "훠원사진", example = "profile.png!")
+    private String memberImage;
+
+    @Schema(description = "회원 닉네임", example = "징징이")
+    private String memberNickname;
+
 }
