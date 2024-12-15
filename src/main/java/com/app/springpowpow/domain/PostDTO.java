4bc1dad28fd28690b6288bcad948397cd90f094c
dev-minchul <minchul766@gmail.com>
2024-12-15 20:59:14 +0900

    file 수정

--- src/main/java/com/app/springpowpow/domain/PostDTO.java
+++ src/main/java/com/app/springpowpow/domain/PostDTO.java
@@ -3,7 +3,10 @@ package com.app.springpowpow.domain;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Component
 @Data
@@ -15,16 +18,8 @@ public class PostDTO {
     private Long memberId;
     @Schema(description = "게시글 내용", example = "테스트 내용1", required = true)
     private String postContent;
-    @Schema(description = "게시글 이미지1", example = "post.png")
-    private String postImage1;
-    @Schema(description = "게시글 이미지2", example = "post.png")
-    private String postImage2;
-    @Schema(description = "게시글 이미지3", example = "post.png")
-    private String postImage3;
-    @Schema(description = "게시글 이미지4", example = "post.png")
-    private String postImage4;
-    @Schema(description = "게시글 이미지5", example = "post.png")
-    private String postImage5;
+    @Schema(description = "게시글 이미지", example = "post.png")
+    private List<MultipartFile> postImage;
     @Schema(description = "게시글 색상", example = "Gold")
     private String postColor;
     @Schema(description = "회원 이메일", example = "test1234@test.app")
