d90ebae8820393bfb1b516d2020ab548c89ac4ec
str-taehyeok <gimtaehyeog63@gmail.com>
2024-12-15 20:24:20 +0900

    데이터 가져옴

--- src/main/java/com/app/springpowpow/repository/PetDAO.java
+++ src/main/java/com/app/springpowpow/repository/PetDAO.java
@@ -34,8 +34,8 @@ public class PetDAO {
     public void deleteAll(Long memberId){
         petMapper.deleteAll(memberId);
     }
-    public void updatePetColor(Long id){
-        petMapper.updateColor(id);
+    public void updatePetColor(PetVO petVO){
+        petMapper.updateColor(petVO);
     }
 
 }
