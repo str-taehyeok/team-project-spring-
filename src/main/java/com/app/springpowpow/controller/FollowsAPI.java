44654f8d2e4287963d1e1f0ee840dbb3888f6f8b
dev-gina <jaleeck1113@gmail.com>
2024-12-18 22:59:17 +0900

    이진아 오후 11시

--- src/main/java/com/app/springpowpow/controller/FollowsAPI.java
+++ src/main/java/com/app/springpowpow/controller/FollowsAPI.java
@@ -4,6 +4,8 @@ import com.app.springpowpow.domain.FollowsDTO;
 import com.app.springpowpow.domain.FollowsVO;
 import com.app.springpowpow.service.FollowsService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +26,8 @@ public class FollowsAPI {
     @ApiResponse(responseCode = "200", description = "팔로우 추가 완료")
     @PostMapping("add")
     public void addFollow(@RequestBody FollowsVO followsVO) {
+//        log.info("팔로우 추가 요청: followerMemberId = {}, followingMemberId = {}, followsFollowState = {}",
+//        followsVO.getFollowerMemberId(), followsVO.getFollowingMemberId(), followsVO.getFollowsFollowState());
         followsService.addFollow(followsVO);
     }
 
@@ -38,6 +42,7 @@ public class FollowsAPI {
     // 팔로워 리스트 조회
     @Operation(summary = "팔로워 리스트 조회", description = "특정 유저의 팔로워 리스트 조회 API")
     @ApiResponse(responseCode = "200", description = "팔로워 리스트 조회 완료")
+    @Parameter(name = "memberId", description = "회원 ID", schema = @Schema(type = "number"))
     @GetMapping("followers/{memberId}")
     public List<FollowsDTO> getFollowerList(@PathVariable Long memberId) {
         return followsService.getFollowerList(memberId);
@@ -46,9 +51,9 @@ public class FollowsAPI {
     // 팔로잉 리스트 조회
     @Operation(summary = "팔로잉 리스트 조회", description = "특정 유저의 팔로잉 리스트 조회 API")
     @ApiResponse(responseCode = "200", description = "팔로잉 리스트 조회 완료")
+    @Parameter(name = "memberId", description = "회원 ID", schema = @Schema(type = "number"))
     @GetMapping("following/{memberId}")
     public List<FollowsDTO> getFollowingList(@PathVariable Long memberId) {
         return followsService.getFollowingList(memberId);
     }
-
 }
